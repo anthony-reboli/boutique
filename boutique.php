f306e0790645a8870fbfc3353dbe9de27907a48b
anthony-reboli <55872129+anthony-reboli@users.noreply.github.com>
2020-03-06 14:22:26 +0100

    boutique fonctionel manque la recherche

--- boutique.php
+++ boutique.php
@@ -1,4 +1,6 @@
-<?php session_start();?>
+<?php session_start();
+	var_dump($_POST);
+?>
 <html>
 <head>
 	<title>Boutique</title>
@@ -12,200 +14,334 @@
 		<?php
 		$bdd= mysqli_connect("localhost","root","","boutique");
 		?>
-		<!-- Choix de categorie -->
-		<label for="categorie-select">Choisir la categorie:</label>
+<!-- Choix de categorie -->
+<label for="categorie-select">Choisir la categorie:</label>
 
-		<form name="form-bout"method="post" type="">
-			<select name="formbou">
-				<option value="sachet">sachets</option>
-				<option value="boite">boites</option>
-				<option value="noire">noire</option>
-				<option value="blanc">blanc</option>
+<form name="form-bout"method="post" type="">
+<select name="pagear" id="categorie-select">
+	<option value="">--choisir une option--</option>
+	<option value="the">the</option>
+	<option value="thehiere">thehiere</option>
+</select>
 
-			</select>
-			<input type="submit" name="catpage">
-			<br>
-		</form>
+					<?php
 
 
 
-		<?php
+$requete4="SELECT * from produits where categories = '1'";
+	 $query4=mysqli_query($bdd, $requete4);
 
+ 				
 
-		$requete4="SELECT * from produits where categories = '1'";
-		$query4=mysqli_query($bdd, $requete4);
+				 if(isset($_POST['catpage'])){ ?>
+					<select name="sc_b" id="categorie-select">
+						
+						<?php if($_POST['pagear'] == 'the'){ ?>
+							
+							<option value="sachet">sachets</option>
+							<option value="boite">boites</option>
+						<?php } ?>
+						<?php if($_POST['pagear'] == 'thehiere'){?>
 
+							<option value="noire">noire</option>
+							<option value="blanc">blanc</option>
+						<?php } ?>
+					</select>
+				<?php } ?>
 
+<input type="submit" name="catpage">
+<br>
+</form>
 
-		?>
-
-
-
-
-
-
-
-		<h1>Notre boutique</h1>
-		<?php 
+<h1>Notre boutique</h1>
+<?php 
 // categoerie
 
+ 
+
+	 
+	
+	 
 
 
+ if(isset($_POST['catpage']))
+	{
 
-
-
-
-
-		if(isset($_POST['catpage']))
+		if ($_POST['pagear'] == 'the' and empty($_POST['sc_b']))
 		{
-			echo "qqqq";
-			echo $_POST['formbou'];
- 		 	if($_POST['formbou'] == 'sachet') // sous categorie sachet active
- 		 	{
- 		 		echo" passe alalala" ;
+			$requete3="SELECT * from produits where categories = '1'";
+	 $query3=mysqli_query($bdd, $requete3);
+	 echo "theee";
+	
+	 
+while($data= mysqli_fetch_assoc($query3))
+	  {
+	  		$i=0;
+	  		$did=$data['id'];
+	  		$img=$data['image'];
+	  		$dnp=$data['nomproduit'];
+	  		var_dump($data);
+	  	echo"<section class=\"thep\">";
+	  	echo" <div class=\"theb\">";
+		// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
+		echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
+		echo "<img class=\"imagebout\" src=\"upload/$img\">";
+		echo "<p>{$data['description']}</p>";
+		echo "<p>{$data['prixproduit']}</p>";
+		
+		
+		echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
+		echo "</div>";
+		echo"</section>";
+		$i++;
 
- 				// header("location:boutique.php");
- 		 		$requete2="SELECT * from produits where categories = '1' and souscategories = 'sachet'";
- 		 		$query2=mysqli_query($bdd, $requete2);
- 		 		while($data= mysqli_fetch_assoc($query2))
- 		 		{
- 		 			$i=0;
- 		 			$did=$data['id'];
- 		 			$img=$data['image'];
- 		 			$dnp=$data['nomproduit'];
- 		 			var_dump($data);
- 		 			echo"<section class=\"thep\">";
- 		 			echo" <div class=\"theb\">";
-					// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
- 		 			echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
- 		 			echo "<img class=\"imagebout\" src=\"upload/$img\">";
- 		 			echo "<p>{$data['description']}</p>";
- 		 			echo "<p>{$data['prixproduit']}</p>";
 
 
- 		 			echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
- 		 			echo "</div>";
- 		 			echo"</section>";
- 		 			$i++;
- 		 		}
- 		 	}
-		 	elseif ($_POST['formbou'] == 'boite') // sous categorie boite active
+
+		
+		
+		
+	  }
+		}
+
+
+		 		 	if ($_POST['pagear'] == 'the' && $_POST['sc_b'] == 'sachet')  // sous categorie sachet active
+		 	{
+		 		echo" passe alalala" ;
+		 
+		 // header("location:boutique.php");
+	 $requete2="SELECT * from produits where categories = '1' and souscategories = 'sachet'";
+	 $query2=mysqli_query($bdd, $requete2);
+	 
+	
+	 
+				while($data= mysqli_fetch_assoc($query2))
+	 	 {
+	  		$i=0;
+	  		$did=$data['id'];
+	  		$img=$data['image'];
+	  		$dnp=$data['nomproduit'];
+	  		var_dump($data);
+	  	echo"<section class=\"thep\">";
+	  	echo" <div class=\"theb\">";
+		// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
+		echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
+		echo "<img class=\"imagebout\" src=\"upload/$img\">";
+		echo "<p>{$data['description']}</p>";
+		echo "<p>{$data['prixproduit']}</p>";
+		
+		
+		echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
+		echo "</div>";
+		echo"</section>";
+		$i++;
+
+
+
+
+		
+		
+		
+	  }
+		 }
+		 elseif ($_POST['pagear'] == 'the' && $_POST['sc_b'] == 'boite')  // sous categorie boite active
 		 	{
 		 		echo" passe boite" ;
-
-		 		// header("location:boutique.php");
-			 	$requete2="SELECT * from produits where categories = '1' and souscategories = 'boite'";
-			 	$query2=mysqli_query($bdd, $requete2);
-			 	while($data= mysqli_fetch_assoc($query2))
-			 	{
-			 		$i=0;
-			 		$did=$data['id'];
-			 		$img=$data['image'];
-			 		$dnp=$data['nomproduit'];
-			 		var_dump($data);
-			 		echo"<section class=\"thep\">";
-			 		echo" <div class=\"theb\">";
-					// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
-			 		echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
-			 		echo "<img class=\"imagebout\" src=\"upload/$img\">";
-			 		echo "<p>{$data['description']}</p>";
-			 		echo "<p>{$data['prixproduit']}</p>";
-
-
-			 		echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
-			 		echo "</div>";
-			 		echo"</section>";
-			 		$i++;
-			 	}
-			}
-			elseif($_POST['formbou'] == "blanc") // sous categorie blanc active
-			{
-				echo " passe blanc" ;
-			 	// header("location:boutique.php");
-				$requete2="SELECT * from produits where categories = '2' and souscategories = 'blanc'";
-				$query2=mysqli_query($bdd, $requete2);
-				while($data2= mysqli_fetch_assoc($query2))
-				{
-					$i=0;
-					$did=$data2['id'];
-					$img=$data2['image'];
-					$dnp=$data2['nomproduit'];
-					var_dump($data2);
-					echo"<section class=\"thep\">";
-					echo" <div class=\"theb\">";
-					// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
-					echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
-					echo "<img class=\"imagebout\" src=\"upload/$img\">";
-					echo "<p>{$data2['description']}</p>";
-					echo "<p>{$data2['prixproduit']}</p>";
-					
-					
-					echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
-					echo "</div>";
-					echo"</section>";
-					$i++;
-				}
-			}
-			elseif ($_POST['formbou'] == "noire") // sous categorie noire active
-			{
-				echo" passe noire" ;
-				// header("location:boutique.php");
-				$requete2="SELECT * from produits where categories = '2' and souscategories = 'noire'";
-				$query2=mysqli_query($bdd, $requete2);
-				while($data2= mysqli_fetch_assoc($query2))
-				{
-					$i=0;
-					$did=$data2['id'];
-					$img=$data2['image'];
-					$dnp=$data2['nomproduit'];
-					var_dump($data2);
-					echo"<section class=\"thep\">";
-					echo" <div class=\"theb\">";
-					// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
-					echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
-					echo "<img class=\"imagebout\" src=\"upload/$img\">";
-					echo "<p>{$data2['description']}</p>";
-					echo "<p>{$data2['prixproduit']}</p>";
-					
-					
-					echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
-					echo "</div>";
-					echo"</section>";
-					$i++;
-				}
-			}
-		}
-		else
-		{
-
-			$requeteb="SELECT * from produits ";
-			$queryb=mysqli_query($bdd, $requeteb);
-			echo "acrako2";
-			while($data= mysqli_fetch_assoc($queryb))
-			{
-				$i=0;
-				$did=$data['id'];
-				$img=$data['image'];
-				$dnp=$data['nomproduit'];
-				var_dump($data);
-				echo"<section class=\"thep\">";
-				echo" <div class=\"theb\">";
-				// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
-				echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
-				echo "<img class=\"imagebout\" src=\"upload/$img\">";
-				echo "<p>{$data['description']}</p>";
-				echo "<p>{$data['prixproduit']}</p>";
-
-
-				echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
-				echo "</div>";
-				echo"</section>";
-				$i++;
-			}
-
-		}
-
+		 
+		 // header("location:boutique.php");
+	 $requete2="SELECT * from produits where categories = '1' and souscategories = 'boite'";
+	 $query2=mysqli_query($bdd, $requete2);
+	 
 	
+	 
+				while($data= mysqli_fetch_assoc($query2))
+	 	 {
+	  		$i=0;
+	  		$did=$data['id'];
+	  		$img=$data['image'];
+	  		$dnp=$data['nomproduit'];
+	  		var_dump($data);
+	  	echo"<section class=\"thep\">";
+	  	echo" <div class=\"theb\">";
+		// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
+		echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
+		echo "<img class=\"imagebout\" src=\"upload/$img\">";
+		echo "<p>{$data['description']}</p>";
+		echo "<p>{$data['prixproduit']}</p>";
+		
+		
+		echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
+		echo "</div>";
+		echo"</section>";
+		$i++;
+
+
+
+
+		
+		
+		
+	  }
+		 }
+		
+		 
+
+		 if ($_POST['pagear'] == 'thehiere'and  empty($_POST['sc_b']))
+		{
+			$requete3="SELECT * from produits where categories = '2'";
+	 $query3=mysqli_query($bdd, $requete3);
+	 echo "thehierree";
+	
+	 
+while($data= mysqli_fetch_assoc($query3))
+	  {
+	  		$i=0;
+	  		$did=$data['id'];
+	  		$img=$data['image'];
+	  		$dnp=$data['nomproduit'];
+	  		var_dump($data);
+	  	echo"<section class=\"thep\">";
+	  	echo" <div class=\"theb\">";
+		// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
+		echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
+		echo "<img class=\"imagebout\" src=\"upload/$img\">";
+		echo "<p>{$data['description']}</p>";
+		echo "<p>{$data['prixproduit']}</p>";
+		
+		
+		echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
+		echo "</div>";
+		echo"</section>";
+		$i++;
+
+
+
+
+		
+		
+		
+	  }
+		}
+
+
+		 		 	elseif($_POST['pagear'] == 'thehiere' && $_POST['sc_b'] == "blanc") // sous categorie sachet active
+		 	{
+		 		echo" passe blanc" ;
+		 
+		 // header("location:boutique.php");
+	 $requete2="SELECT * from produits where categories = '2' and souscategories = 'blanc'";
+	 $query2=mysqli_query($bdd, $requete2);
+	 
+	
+	 
+				while($data2= mysqli_fetch_assoc($query2))
+	 	 {
+	  		$i=0;
+	  		$did=$data2['id'];
+	  		$img=$data2['image'];
+	  		$dnp=$data2['nomproduit'];
+	  		var_dump($data2);
+	  	echo"<section class=\"thep\">";
+	  	echo" <div class=\"theb\">";
+		// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
+		echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
+		echo "<img class=\"imagebout\" src=\"upload/$img\">";
+		echo "<p>{$data2['description']}</p>";
+		echo "<p>{$data2['prixproduit']}</p>";
+		
+		
+		echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
+		echo "</div>";
+		echo"</section>";
+		$i++;
+
+
+
+
+		
+		
+		
+	  }
+		 }
+		 elseif ($_POST['pagear'] == 'thehiere' && $_POST['sc_b'] == 'noire')  // sous categorie noire active
+		 	{
+		 		echo" passe noire" ;
+		 
+		 // header("location:boutique.php");
+	 $requete2="SELECT * from produits where categories = '2' and souscategories = 'noire'";
+	 $query2=mysqli_query($bdd, $requete2);
+	 
+	
+	 
+				while($data2= mysqli_fetch_assoc($query2))
+	 	 {
+	  		$i=0;
+	  		$did=$data2['id'];
+	  		$img=$data2['image'];
+	  		$dnp=$data2['nomproduit'];
+	  		var_dump($data2);
+	  	echo"<section class=\"thep\">";
+	  	echo" <div class=\"theb\">";
+		// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
+		echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
+		echo "<img class=\"imagebout\" src=\"upload/$img\">";
+		echo "<p>{$data2['description']}</p>";
+		echo "<p>{$data2['prixproduit']}</p>";
+		
+		
+		echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
+		echo "</div>";
+		echo"</section>";
+		$i++;
+
+
+
+
+		
+		
+		
+	  }
+	}
+}
+		
+	
+			
+	
+	else {
+
+		$requeteb="SELECT * from produits ";
+	 $queryb=mysqli_query($bdd, $requeteb);
+	 echo "acrako2";
+		while($data= mysqli_fetch_assoc($queryb))
+	  {
+	  		$i=0;
+	  		$did=$data['id'];
+	  		$img=$data['image'];
+	  		$dnp=$data['nomproduit'];
+	  		var_dump($data);
+	  	echo"<section class=\"thep\">";
+	  	echo" <div class=\"theb\">";
+		// echo "<h1>{<a href =\"panier.php?id=$did\">$data['nomproduit']}</h1></a>";
+		echo "<a href=\"profilitem.php?p=$did\">$dnp</a> /";
+		echo "<img class=\"imagebout\" src=\"upload/$img\">";
+		echo "<p>{$data['description']}</p>";
+		echo "<p>{$data['prixproduit']}</p>";
+		
+		
+		echo "<form method=\"post\" action=\"panier.php?id=$did\"><input type=\"submit\" name=\"boutiqueb[$i]\"></form>";
+		echo "</div>";
+		echo"</section>";
+		$i++;
+
+
+
+
+		
+		
+		
+	  }
+	  
+	}
 	// else{
 	// 	include('boutique2.php');
 	// }
@@ -216,34 +352,34 @@
 
 
 
+	
+	
+
+		 $requete="SELECT * from produits where categories = 2";
+	 $query=mysqli_query($bdd, $requete);
+	 $reponse=mysqli_fetch_all($query);
+
+?>
 
 
 
-		$requete="SELECT * from produits where categories = 2";
-		$query=mysqli_query($bdd, $requete);
-		$reponse=mysqli_fetch_all($query);
-
-		?>
+	
 
 
 
+	<?php
+
+
+	    
+
+	?>
+
+
+	
 
 
 
+	
 
-		<?php
-
-
-
-
-		?>
-
-
-
-
-
-
-
-
-	</body>
-	</html>
+</body>
+</html>
