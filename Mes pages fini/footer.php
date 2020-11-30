9e7aa3ab8dc756c6b253b43f35f668e11c395d43
Anthony REBOLI <anthony.reboli@nablaware.fr>
2020-11-30 09:37:31 +0100

    debut de mise a jour

--- Mes pages fini/footer.php
+++
@@ -1,7 +0,0 @@
- <footer id="foot">
-      <a href="https://fr-fr.facebook.com/"><img width="100px" height="100px" id="logo"  src="upload/facebook-logo.jpg"></a>
-      <a href="https://www.instagram.com"><img width="100px" height="100px" class="logo" src="upload/instagram-logo.jfif"></a>
-      <a href="https://www.linkedin.com"><img width="100px" height="100px" class="logo" src="upload/linkedin.jpg"></a>
-      <a href="https://www.twitter.com"><img width="100px" height="100px" class="logo" src="upload/twitter-logo.jpg"></a>
-      <p >Copyright 2020 Coding School | All Rights Reserved | Project by Anthony & Gregory.</p> 
- </footer>
